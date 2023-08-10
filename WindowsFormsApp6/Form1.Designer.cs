85a9d7009448582379d95efd981687be7f6814da
Marwan <Marwan@DESKTOP-K4ABQD8>
2023-08-10 17:14:27 +0100

    Add project files.

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(236, 748);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 671);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 55);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Scan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(25, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(186, 284);
+            this.listBox1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(234, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1146, 745);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1146, 745);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1387, 748);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TwainScanning;
+using TwainScanning.Bridgex86;
+using TwainScanning.Capability.CapabilitySets;
+using TwainScanning.NativeStructs;
+using WindowsFormsApp6.Properties;
+
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        private DataSourceManager dataSourceManager;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            dataSourceManager = new DataSourceManager();
+        }
+        private List<TwIdentity> loadSources()
+        {
+            var list = new List<TwIdentity>();
+            var scanners = dataSourceManager.AvailableSources();
+            foreach (var item in scanners)
+            {
+                list.Add(item); 
+            }
+            return list;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var list = loadSources();
+            foreach(var item in list)
+            {
+                listBox1.Items.Add(item.ProductName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           string outPut = @"C:\\Users\\Marwan\\Documents\\ScannedImages"+GenerateNameId()+".jpeg";
+            var settings = new ScanSettings();
+            settings.Device = listBox1.SelectedItem.ToString(); // default scanner is used if omitted
+            settings.TransferMechanism = TwSX.Native;
+            settings.ShowUI = false; // show UI from driver software or not
+            settings.CloseUIAfterAcquire = true; // close UI from driver software or not
+            var pageCap = Bridgex86.GetSupportedPageSizes(listBox1.SelectedItem.ToString());
+            
+            
+            settings.AutoFeed = true;
+            settings.ColorMode = TwPixelType.RGB;
+            settings.DuplexEnabled = true;
+            settings.ImageCount = -1; // scan all available images or limit number of images to 
+            
+            settings.ImageQuality = 80;
+            settings.Resolution = new ScanResolution(300); // resolution can be individual for x 
+           
+           settings.MultiPageScan = false; // scan into single file with multiple pages (tiff and 
+            
+
+           
+           Bridgex86.Acquire(outPut,settings);
+           pictureBox1.Image = Image.FromFile(outPut);
+           pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+
+        }
+        private string GenerateNameId()
+        {
+           return Guid.NewGuid().ToString();  
+        }
+    }
+}
